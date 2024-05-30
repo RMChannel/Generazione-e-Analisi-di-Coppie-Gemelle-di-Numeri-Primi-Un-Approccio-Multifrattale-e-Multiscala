51bf1be79a3b6738ad0286a1239ac9ed9e4e1390
RMChannel <98057423+RMChannel@users.noreply.github.com>
2024-05-30 23:05:10 +0200

    Checkpoint

--- main.c
+++ main.c
@@ -30,57 +30,53 @@ int ifNotExist(int *arr, int n, int target) {
     while (left <= right) {
         int mid = left + (right - left) / 2;
         if (arr[mid] == target) {
-            return mid;
+            return 0;
         } else if (arr[mid] < target) {
             left = mid + 1;
         } else {
             right = mid - 1;
         }
     }
-    return 0;
+    return 1;
 }
 
 int calcolaFamiglie(int *primes, int *notprimes, int m, int *foundPrimes, int n, int i) {
     int limit=m, first=1;
-    int a,b,k,control;
-    int nexa;
-     for(a=0,b=0,k=1,n=m*k;(a<=limit && b<=limit);k++) {
-        b=(m*k)-1;
+    int a,b,k;
+     for(a=0,b=0,k=1,n=m*k;(a<=limit && b<=limit);k++,n=m*k) {
+        b=n-1;
         if(controlloPrime(primes,b)) {
             inserisci_ordinato(primes,&i1,b);
-            if(!ifNotExist(notprimes,i2,b)) inserisci_ordinato(notprimes,&i2,b);
+            if(ifNotExist(notprimes,i2,b)) inserisci_ordinato(notprimes,&i2,b);
             (*foundPrimes)++;
         }
-        a=(m*k)+1;
-        nexa = ifNotExist(notprimes,i2,a);
+        a=n+1;
         if(controlloPrime(primes,a)) {
             inserisci_ordinato(primes,&i1,a);
-            if(!nexa) inserisci_ordinato(notprimes,&i2,a);
             (*foundPrimes)++;
         }
-        else if(!nexa) inserisci_ordinato(notprimes,&i2,a);
+        if(ifNotExist(notprimes,i2,a)) inserisci_ordinato(notprimes,&i2,a);
         if(first) {
             limit*=primes[i];
             first=0;
         }
      }
-    for (int inti = 0; notprimes[i] < m && i < i2; i++) {
-        a = 0;
+    for (; notprimes[i] < m && i < i2; i++) {
         k = 1;
-        while (a < limit) {
+        do {
             a = (m * k) + notprimes[i];
             k++;
             if (controlloPrime(primes, a)) {
-                if (!ifNotExist(primes, i1, a)) {
+                if (ifNotExist(primes, i1, a)) {
                     inserisci_ordinato(primes, &i1, a);
                     (*foundPrimes)++;
                 }
             } 
 
-            if (!ifNotExist(notprimes, i2, a)) {
+            if (ifNotExist(notprimes, i2, a)) {
                 inserisci_ordinato(notprimes, &i2, a);
             }
-        }
+        }while (a < limit);
     }
     return limit;
 }
@@ -91,18 +87,17 @@ void findPrimes(int *primes, int *notprimes, int n) {
         m=calcolaFamiglie(primes,notprimes,m,&foundPrimes,n,i);
         i++;
     }
-    printArray(primes,n);
+    //printArray(primes,n);
     printf("%d\n",foundPrimes);
 }
 
 int main() {
-    int *primes=malloc(N*sizeof(int));
-    int *notprimes=malloc(N*sizeof(int));
+    int n=10000;
+    size_t size=N*sizeof(int);
+    int *primes=malloc(size);
+    int *notprimes=malloc(size);
     notprimes[0]=4;
     primes[0]=2;
-    int n=1000;
     findPrimes(primes,notprimes,n);
-    free(primes);
-    free(notprimes);
     return 0;
 }
